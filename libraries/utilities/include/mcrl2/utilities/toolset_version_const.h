412a312022af5f016030a0fa8b6e52c92a8ce82b
Wieger Wesselink <j.w.wesselink@tue.nl>
2018-04-09 11:42:14 +0200

    - Added version file.

---
+++ libraries/utilities/include/mcrl2/utilities/toolset_version_const.h
@@ -0,0 +1,14 @@
+#ifndef MCRL2_UTILITIES_TOOLSET_VERSION_CONST_H
+#define MCRL2_UTILITIES_TOOLSET_VERSION_CONST_H
+#include <string>
+
+namespace mcrl2 {
+
+namespace utilities {
+
+  const std::string MCRL2_VERSION = "2018.0";
+
+}
+
+}
+#endif // MCRL2_UTILITIES_TOOLSET_VERSION_CONST_H
