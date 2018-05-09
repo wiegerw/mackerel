41bd83df029d353bbbae4fcc85c07a380f73ae90
Wieger Wesselink <j.w.wesselink@tue.nl>
2018-05-09 14:58:37 +0200

    - Removed class basic_rewriter.

--- libraries/data/include/mcrl2/data/rewriter.h
+++ libraries/data/include/mcrl2/data/rewriter.h
@@ -12,17 +12,12 @@
 #ifndef MCRL2_DATA_REWRITER_H
 #define MCRL2_DATA_REWRITER_H
 
-#include <algorithm>
-#include <functional>
-#include <iostream>
-#include <sstream>
+// TODO: this include should not be necessary, it is a problem in the design of the data library
+#include "mcrl2/data/expression_traits.h"
 
 #include "mcrl2/data/data_specification.h"
 #include "mcrl2/data/detail/rewrite.h"
-#include "mcrl2/data/expression_traits.h"
-#include "mcrl2/data/replace.h"
 #include "mcrl2/data/substitutions/mutable_indexed_substitution.h"
-#include "mcrl2/utilities/exception.h"
 
 namespace mcrl2
 {
@@ -30,69 +25,33 @@ namespace mcrl2
 namespace data
 {
 
-/// \brief Rewriter class for the mCRL2 Library. It only works for terms of type data_expression.
-// TODO: The rewriter only supports data::mutable_indexed_substitution<> as substitution type. This
-// needs to be fixed, because it is a major source of inefficiency.
-// TODO: The rewriter is implemented in terms of detail::Rewriter. This needs to be fixed, because
-// the class detail::Rewriter is unspecified, and has several problems.
-template < typename Term >
-class basic_rewriter
-{
-  public:
-    /// \brief The type for the substitution that is used internally.
-    typedef data::mutable_indexed_substitution<> substitution_type;
-
-  protected:
-    /// \brief The wrapped Rewriter.
-    std::shared_ptr<detail::Rewriter> m_rewriter;
-
-  public:
-
-    /// \brief The type for expressions manipulated by the rewriter.
-    typedef Term term_type;
-
-    /// \brief The rewrite strategies of the rewriter.
-    typedef rewrite_strategy strategy;
-
-  protected:
-
-    /// \brief Constructor.
-    /// \param[in] r A rewriter
-    explicit basic_rewriter(const std::shared_ptr<detail::Rewriter>& r) :
-      m_rewriter(r)
-    {}
-
-    /// \brief Copy Constructor
-    basic_rewriter(const basic_rewriter& other)=default;
-
-    /// \brief Assignment operator
-    basic_rewriter& operator=(const basic_rewriter& other)=default;
-
-    /// \brief Constructor.
-    /// \param[in] dataspec A data specification
-    /// \param[in] s A rewriter strategy.
-    explicit basic_rewriter(const data_specification& dataspec, strategy s = jitty) :
-      m_rewriter(detail::createRewriter(dataspec, used_data_equation_selector(dataspec), s))
-    { }
-
-    /// \brief Constructor.
-    basic_rewriter(const data_specification& dataspec, const used_data_equation_selector& equation_selector, strategy s = jitty) :
-      m_rewriter(detail::createRewriter(dataspec, equation_selector, s))
-    {}
-
-};
-
 /// \brief Rewriter that operates on data expressions.
 //
 /// \attention As long as normalisation of sorts remains necessary, the data
 /// specification object used for construction *must* exist during the
 /// lifetime of the rewriter object.
-
-class rewriter: public basic_rewriter<data_expression>
+//
+// TODO: The rewriter only supports data::mutable_indexed_substitution<> as substitution type. This
+// needs to be fixed, because it is a major source of inefficiency.
+// TODO: The rewriter is implemented in terms of detail::Rewriter. This needs to be fixed, because
+// the class detail::Rewriter is unspecified, and has several problems.q
+class rewriter
 {
-  public:
-    typedef basic_rewriter<data_expression>::substitution_type substitution_type;
+  protected:
+    /// \brief The wrapped Rewriter.
+    std::shared_ptr<detail::Rewriter> m_rewriter;
 
+  public:
+    /// \brief The type for the substitution that is used internally.
+    typedef data::mutable_indexed_substitution<> substitution_type;
+
+    /// \brief The type for expressions manipulated by the rewriter.
+    typedef data_expression term_type;
+
+    /// \brief The rewrite strategies of the rewriter.
+    typedef rewrite_strategy strategy;
+
+  public:
     /// \brief Copy constructor.
     /// \param[in] r a rewriter.
     rewriter(const rewriter& r) = default;
@@ -100,8 +59,8 @@ class rewriter: public basic_rewriter<data_expression>
     /// \brief Constructor.
     /// \param[in] dataspec A data specification
     /// \param[in] s A rewriter strategy.
-    explicit rewriter(const data_specification& dataspec = rewriter::default_specification(), strategy s = jitty) :
-      basic_rewriter<data_expression>(dataspec, s)
+    explicit rewriter(const data_specification& dataspec = rewriter::default_specification(), strategy s = jitty):
+      m_rewriter(detail::createRewriter(dataspec, used_data_equation_selector(dataspec), s))
     { }
 
     /// \brief Constructor.
@@ -109,10 +68,9 @@ class rewriter: public basic_rewriter<data_expression>
     /// \param[in] selector A component that selects the equations that are converted to rewrite rules
     /// \param[in] s A rewriter strategy.
     template < typename EquationSelector >
-    rewriter(const data_specification& dataspec, const EquationSelector& selector, strategy s = jitty) :
-      basic_rewriter<data_expression>(dataspec, selector, s)
-    {
-    }
+    rewriter(const data_specification& dataspec, const EquationSelector& equation_selector, strategy s = jitty) :
+      m_rewriter(detail::createRewriter(dataspec, equation_selector, s))
+    { }
 
     /// \brief Default specification used if no specification is specified at construction
     static data_specification& default_specification()
@@ -131,7 +89,7 @@ class rewriter: public basic_rewriter<data_expression>
       return m_rewriter->rewrite(x, sigma);
     }
 
-    /// \brief Rewrites the data expression x, and on the fly applies a substitution function
+    /// \brief Rewrites the data expression x, and on the fly applies a substitution
     /// to data variables.
     /// \param[in] x A data expression
     /// \param[in] sigma A substitution function
@@ -148,7 +106,7 @@ class rewriter: public basic_rewriter<data_expression>
       return m_rewriter->rewrite(x, sigma_copy);
     }
 
-    /// \brief Rewrites the data expression d, and on the fly applies a substitution function
+    /// \brief Rewrites the data expression x, and on the fly applies a substitution
     /// to data variables.
     /// \param[in] x A data expression
     /// \param[in] sigma A substitution function
